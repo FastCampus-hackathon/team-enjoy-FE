b31be2df8251d7739332ff92a9fc8ad1fe6306dd
0andme <yu00mi97@gmail.com>
2022-02-22 04:12:45 +0900

    feat : 사이드바 데이터 추가

---
+++ src/data/sidebarList.js
@@ -0,0 +1,58 @@
+const sidebarList = [
+  {
+    title: "MY홈",
+    isActive: false,
+    isToggle: false,
+    src: "home",
+  },
+  {
+    title: "이력서",
+    isActive: false,
+    isToggle: true,
+    src: "resume",
+  },
+  {
+    title: "스크랩/관심기업",
+    isActive: true,
+    isToggle: false,
+    src: "scrap",
+  },
+  {
+    title: "지원할 만한 공고",
+    isActive: false,
+    isToggle: true,
+    src: "thumbUp",
+  },
+  {
+    title: "이직 제안",
+    isActive: false,
+    isToggle: true,
+    src: "jobProposal",
+  },
+  {
+    title: "지원내역",
+    isActive: false,
+    isToggle: true,
+    src: "history",
+  },
+  {
+    title: "기업 알아보기",
+    isActive: false,
+    isToggle: false,
+    src: "findEnterprise",
+  },
+  {
+    title: "진단/검사",
+    isActive: false,
+    isToggle: true,
+    src: "test",
+  },
+  {
+    title: "면접현황",
+    isActive: false,
+    isToggle: false,
+    src: "interview",
+  },
+];
+
+export default sidebarList;
