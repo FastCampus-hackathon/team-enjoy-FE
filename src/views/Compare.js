ad9b8e2fa6d65be7061f5880eae87b58f3df6841
0andme <yu00mi97@gmail.com>
2022-02-22 04:13:18 +0900

    feat :비교 뷰 페이지에 사이드 바 추가

--- src/views/Compare.js
+++ src/views/Compare.js
@@ -1,10 +1,12 @@
 import Header from "components/Header";
+import SideBar from "components/SideBar";
 import React, { Fragment } from "react";
 
 function Compare() {
   return (
     <Fragment>
       <Header />
+      <SideBar />
     </Fragment>
   );
 }
