f685b8686472d19412efaed73e9674ea20a27b53
sieun <sieun0115@ajou.ac.kr>
2022-02-22 00:10:05 +0900

    feat: 검색어 검색 바 컴포넌트 추가

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Compare from "views/Compare";
+import SearchBar from "components/SearchBar";
 
 function AllRoutes() {
   return (
     <Routes>
+      <Route path="/" element={<SearchBar />} />
       <Route path="/compare" element={<Compare />} />
     </Routes>
   );
