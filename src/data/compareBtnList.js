720f194329fa3740bce4ba7118dfa048740649c9
0andme <yu00mi97@gmail.com>
2022-02-23 00:42:00 +0900

    feat : 태그 리스트 템플릿 추가

--- src/data/compareBtnList.js
+++ src/data/compareBtnList.js
@@ -1,2 +1,22 @@
-const compareBtnList = ["마감순", "지역별", "경력별", "근무형태", "연봉범위"];
+const compareBtnList = [
+  {
+    title: "학력별",
+    detailList: {
+      any: "학력무관",
+      highschool: "고등학교졸업이상",
+      univ: "대학졸업(2,3년)이상",
+      "univ-4year": "대학교졸업(4년)이상",
+    },
+  },
+  {
+    title: "경력별",
+    detailList: {
+      any_experience: "경력무관",
+      both_experience: "신입/경력",
+      experience: "경력",
+      new_experience: "신입",
+    },
+  },
+];
+
 export default compareBtnList;
